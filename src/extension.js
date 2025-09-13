46ef86a93ece38a890d490e86130cb7dbee5d2ef
Jacobtread <jacobtread@gmail.com>
2025-09-13 13:44:16 +1200

    feat: update extension for new zed folder icons format

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import icons from "./icons.js";
+import { icons, folderIcons } from "./icons.js";
 import fileExtensions from "./icons/fileExtensions.js";
 import fileNames from "./icons/fileNames.js";
 import folderNames from "./icons/folderNames.js";
@@ -8,35 +8,36 @@ import { makeFolderIconPairs } from "./helper.js";
 
 const folderPairs = makeFolderIconPairs(
     folderNames,
-    folderNamesExpanded
-)
+    folderNamesExpanded,
+    folderIcons,
+);
 
 const iconTheme = {
-  $schema: "https://zed.dev/schema/icon_themes/v0.2.0.json",
-  name: "Clean VSCode Icon Theme",
-  author: "Jacobtread",
-  themes: [
-    {
-      name: "Clean VSCode Icon Theme",
-      appearance: "dark",
-      directory_icons: {
-        collapsed: "./icons/folder.svg",
-        expanded: "./icons/folder_open.svg",
-      },
-      named_directory_icons: folderPairs,
-      file_stems: fileNames,
-      file_suffixes: fileExtensions,
-      file_icons: icons,
-    },
-  ],
+    $schema: "https://zed.dev/schema/icon_themes/v0.2.0.json",
+    name: "Clean VSCode Icon Theme",
+    author: "Jacobtread",
+    themes: [
+        {
+            name: "Clean VSCode Icon Theme",
+            appearance: "dark",
+            directory_icons: {
+                collapsed: "./icons/folder.svg",
+                expanded: "./icons/folder_open.svg",
+            },
+            named_directory_icons: folderPairs,
+            file_stems: fileNames,
+            file_suffixes: fileExtensions,
+            file_icons: icons,
+        },
+    ],
 };
 
 fs.writeFile(
-  "icon_themes/vscode-icon-theme.json",
-  JSON.stringify(iconTheme, undefined, 4),
-  (err) => {
-    if (err) {
-      console.log("error", err);
-    }
-  }
+    "icon_themes/vscode-icon-theme.json",
+    JSON.stringify(iconTheme, undefined, 4),
+    (err) => {
+        if (err) {
+            console.log("error", err);
+        }
+    },
 );
--- src/helper.js
+++ src/helper.js
@@ -1,15 +1,13 @@
-
 /**
  * @param {String[]} itemList
  * @param {String} scope
  * @returns
  */
 export const make = (itemList, scope) => {
-  let obj = {};
-  itemList.forEach((item) => (obj = { [item]: scope, ...obj }));
-  return obj;
+    let obj = {};
+    itemList.forEach((item) => (obj = { [item]: scope, ...obj }));
+    return obj;
 };
-
 
 /**
  * Combine the collapsed and expanded variants of icons for
@@ -17,8 +15,9 @@ export const make = (itemList, scope) => {
  *
  * @param {Record<string, string>} collapsed
  * @param {Record<string, string>} expanded
+ * @param {Record<string, { path: string }>} icons
  */
-export const makeFolderIconPairs = (collapsed, expanded) => {
+export const makeFolderIconPairs = (collapsed, expanded, icons) => {
     const pairs = {};
 
     const collapsedKeys = Object.keys(collapsed);
@@ -26,17 +25,40 @@ export const makeFolderIconPairs = (collapsed, expanded) => {
 
     const combinedKeys = new Set([...collapsedKeys, ...expandedKeys]);
 
-    const sortedKeys = [...combinedKeys.values()]
-        .sort();
-
+    const sortedKeys = [...combinedKeys.values()].sort();
 
     for (const key of sortedKeys) {
         const collapsedName = collapsed[key];
         const expandedName = expanded[key];
 
+        let collapsedIcon = icons[collapsedName];
+        let expandedIcon = icons[expandedName];
 
-        pairs[key] = { collapsed: collapsedName, expanded: expandedName };
+        if (collapsedIcon === undefined && expandedIcon === undefined) {
+            console.warn(
+                `folder icons are undefined for '${collapsedName}' and '${expandedName}`,
+            );
+            continue;
+        }
+
+        if (collapsedIcon === undefined) {
+            collapsedIcon = expandedIcon;
+            console.warn(
+                `folder icon collapsed variant is undefined for '${collapsedName}' using expanded variant`,
+            );
+        }
+        if (expandedIcon === undefined) {
+            expandedIcon = collapsedIcon;
+            console.warn(
+                `folder icon expanded variant is undefined for '${expandedName}' using collapsed variant`,
+            );
+        }
+
+        pairs[key] = {
+            collapsed: collapsedIcon.path,
+            expanded: expandedIcon.path,
+        };
     }
 
     return pairs;
-}
+};
